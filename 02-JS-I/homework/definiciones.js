32a568bae2b163b52e24618c715333ecfd2d16b0
estebanedcm <estebanedcm@gmail.com>
2021-06-28 01:13:00 -0300

    add file with definitions of the homwork

---
+++ 02-JS-I/homework/definiciones.js
@@ -0,0 +1,14 @@
+	* Variables
+son datos que tienen un nombre y pueden cambiar de valor y se usa mucho en programacion
+
+	* Strings
+son un tipo de dato de las variables, el tipo cadena de carateres o texto
+
+	* Funciones (argumentos, `return`)
+son un conjunto cerrado de instrucciones, al ser serrado para introducir datos se usan los argumentos, y para sacar datos el comando retorno dentro de la funcion
+
+	* Declaraciones `if`
+es un condicional que evalua una condicion
+
+	* Valores booleanos (`true`, `false`)
+es un tipo de dato que se devuelve al evaluar una condicion 
