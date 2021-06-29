237f66ff546c29602fbffd504fe72f8e9cfd4371
estebanedcm <estebanedcm@gmail.com>
2021-06-29 19:30:00 -0300

    homework 3 finished

---
+++ 03-JS-II/homework/definiciones.js
@@ -0,0 +1,9 @@
+definiciones
+
+for
+
+es una herramienta de javascript que nos permiete repetir una instruccion un mumero limitado y sabido de antemano de veces
+
+&&, ||, !
+
+son operadores logicos donde se comparan valores booleanos de datos y variables
