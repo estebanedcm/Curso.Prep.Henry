1c2eeb0148478a7b49c6559f636517e8eb485801
estebanedcm <estebanedcm@gmail.com>
2021-06-30 03:05:27 -0300

    definiciones agregadas del JS IV

---
+++ 05-JS-IV/homework/definiciones.js
@@ -0,0 +1,11 @@
+
+	* Objetos
+		son grupos de propiedades con sus respectivos valores
+	* Propiedades
+		son atributos de un ibjeto que puede tener un valor
+	* Métodos
+		son funciones pero que estan dentro de un objeto
+	* Bucle `for…in`
+		un ciclo for para recorrer las propiedades dentro de un objeto
+	* Notación de puntos vs notación de corchetes
+		la notacion de punto simplifica el acceso a las propiedades de los objetos, pero es literal, si queremos usar una variable para acceder a una propiedad de un objeto debemos usar la notacion por corchetes
