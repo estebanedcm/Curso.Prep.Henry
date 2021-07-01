7f1844b9f2f57d351c8e0c1c0e510ad6bd529f58
estebanedcm <estebanedcm@gmail.com>
2021-07-01 21:35:04 +0000

    homework V finished

---
+++ 06-JS-V/homework/definiciones.js
@@ -0,0 +1,6 @@
+prototype 
+	es un herramienta para agregar metodos a una clase y a sus instancias
+	
+constructors
+
+	son metodos que crean objetos y crean clases que pueden heredad atributos de otros objetos y clases
